c36ff306e07cfa7e9158135a3c3b11a9f5fd6089
obman <deniss9oo@gmail.com>
2017-06-29 23:10:50 +0200

    Started developing IPSC web mobile app, initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fly
+ * Date: 29.6.2017
+ * Time: 22:48
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>IPSC training mobile helper application</title>
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<main>
+    <div class="ipsc-trainings--wrapper">
+        <section class="ipsc-trainings--drill">
+            <header>
+                <h2>Aimbot Drill - Target Acquisition Speed</h2>
+                <p>A simple yet effective shooting drill to practice fast target identification/acquisition.</p>
+            </header>
+            <div>
+                <form id="ipsc-aimbot-drill">
+                    <input type="range" name="ipsc-aimbot-array-1" min="1" max="10">
+                </form>
+            </div>
+        </section>
+    </div>
+</main>
+</body>
+</html>
